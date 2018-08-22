3ab51e85e094372e7ca8aaa9d1419746fbad82c9
jamesnagle <james.rnagle@gmail.com>
2018-08-22 11:28:44 -0400

    Initial commit

---
+++ index.js
@@ -0,0 +1,6 @@
+const express = require('express');
+const app = express();
+
+app.get('/', (req, res) => res.send('Hey Vlad, Brewskies live!'));
+
+app.listen(3000, () => console.log('Listening on port 3000'));
