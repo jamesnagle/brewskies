92168107c2b58ceba2db2dc2c3d2606cdedd25c5
jamesnagle <james.rnagle@gmail.com>
2018-08-25 09:38:02 -0400

    Initial Mongo install

--- index.js
+++ index.js
@@ -1,6 +1,16 @@
 const express = require('express');
 const app = express();
 
-app.get('/', (req, res) => res.send('Hey James, Brewskies is indeed live!'));
+const mongoose = require('mongoose');
+mongoose.connect('mongodb://127.0.0.1/test');
+const db = mongoose.connection;
 
-app.listen(3000, () => console.log('Listening on port 3000'));
+db.on('error', console.error.bind(console, 'connection error:'));
+
+db.once('open', function () {
+    console.log('DB connected!');
+
+    app.get('/', (req, res) => res.send('Hey James, Brewskies is indeed live!'));
+
+    app.listen(3000, () => console.log('Listening on port 3000'));
+});
