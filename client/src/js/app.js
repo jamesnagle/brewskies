cd6a8881e35749382e45b0677c6207b11a9157e7
jamesnagle <james.rnagle@gmail.com>
2018-08-26 15:01:24 -0400

    debug

--- client/src/js/app.js
+++ client/src/js/app.js
@@ -18,7 +18,7 @@ export default class App extends React.Component {
     render() {
         return (
             <ul>
-                {this.state.breweries.map(function (brewery) {
+                {this.state.breweries.forEach((brewery) => {
                     <li>{brewery.name}</li>
                 })}
             </ul>
