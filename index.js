5f19906979b189de863d6c2a3ffd1460a8aefd25
jamesnagle <james.rnagle@gmail.com>
2018-08-26 13:57:50 -0400

    add mongoose logging

--- index.js
+++ index.js
@@ -3,6 +3,7 @@ const app = express();
 
 const mongoose = require('mongoose');
 mongoose.connect('mongodb://127.0.0.1/brews');
+mongoose.set('debug', true);
 const db = mongoose.connection;
 
 db.on('error', console.error.bind(console, 'connection error:'));
